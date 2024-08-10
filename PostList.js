21dc9b7cc877627a942e553fbc0302f0ad977f14
Chikarakobu04 <san9dai2004.2@gmail.com>
2024-08-10 16:53:27 +0900

    modify postlist

--- PostList.js
+++ PostList.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PostList.css';
 
 function PostList({ messages }) {
+  const [searchQuery, setSearchQuery] = useState(''); // 検索クエリのステート
+
+  // 検索クエリに基づいてメッセージをフィルタリング
+  const filteredMessages = messages.filter((message) =>
+    message.text.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className="post-list">
-      <h1>投稿一覧</h1>
+      
+      <div className="search-bar">
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search messages..."
+        />
+      </div>
       <ul>
-        {messages.map((message, index) => (
-          <li key={index}>
-            <h2>投稿 {index + 1}</h2>
-            {message.image && (
-              <img src={message.image} alt="投稿された画像" className="uploaded-image" />
-            )}
-            <p>{message.text}</p>
+        {filteredMessages.map((message, index) => (
+          <li key={index} className="post-item">
+            <div className="post-content">
+              <div className="message-text">
+                <p>{message.text}</p>
+              </div>
+              {message.image && (
+                <div className="image-container">
+                  <img src={message.image} alt="投稿された画像" className="uploaded-image" />
+                </div>
+              )}
+            </div>
           </li>
         ))}
       </ul>
