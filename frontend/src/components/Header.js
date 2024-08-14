9c8bf1e3b0cdfa0119c9828467070e4eadd70bfd
ShunyaNagashige <nagashige190441091@gmail.com>
2024-08-14 19:44:48 +0900

    探す・投稿ボタンの追加 #7

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faSearch, faEdit, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faSearch, faEdit, faSignInAlt, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
 
 function Header({ onNavClick }) {
@@ -10,6 +10,14 @@ function Header({ onNavClick }) {
         <button onClick={() => onNavClick('home')}>
           <FontAwesomeIcon icon={faHome} size="2x" />
           <span>ホーム</span>
+        </button>
+        <button onClick={() => onNavClick('')}>
+          <FontAwesomeIcon icon={faSearch} size="2x" />
+          <span>探す</span>
+        </button>
+        <button onClick={() => onNavClick('messageBoard')}>
+          <FontAwesomeIcon icon={faPaperPlane} size="2x" />
+          <span>投稿</span>
         </button>
         <button onClick={() => onNavClick('login')}>
           <FontAwesomeIcon icon={faSignInAlt} size="2x" />
